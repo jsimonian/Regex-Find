0dac99431f9d1dcd20a7c0fb27c87428c034570d
Joseph Simonian <j.simonian10@gmail.com>
2015-08-04 00:23:01 -0700

    First draft. Tons of edits. See description.
    Set up the real way of finding stuff, removed old system (button press
    and keyboard shortcut should be the same). Prompts with dialog box,
    then highlights all matched patterns. Super slow.

--- Regex-Find.safariextension/search.js
+++ Regex-Find.safariextension/search.js
@@ -1,25 +1,51 @@
 safari.self.addEventListener("message", handleMessage, false);
 
 function handleMessage(event) {
+
     if (event.name == "search") {
+
         var input = prompt("Enter your search term");
-        var loc = document.body.innerText.search(input);
-        if (loc != -1) {
-            alert("Found!");
+        var reg = new RegExp(input, "g"); // Force to global
+        var text = document.body.innerHTML;
+
+        var matches = [];
+        var match = reg.exec(text);
+
+        while (match != null) {
+            matches.push(match);
+            match = reg.exec(text);
         }
-        else {
-            alert("Not Found.");
+
+        for (m of matches.uniques()) {
+            if (m != undefined) {
+                highlight(m);
+            }
+        }
+
+    }
+}
+
+function highlight(text) {
+    if (window.find && window.getSelection) {
+        var sel = window.getSelection();
+        sel.collapse(document.body, 0);
+
+        while (window.find(text)) {
+            document.designMode = "on";
+            document.execCommand("hiliteColor", false, "yellow");
+            document.designMode = "off";
+            sel.collapseToEnd();
         }
     }
 }
 
-function parse() {
-    return document.body.textContent;
-}
-
-function search(main, elm) {
-    if (str1.toLowerCase().contains(str2.toLowerCase())) {
-        return true;
-    }
-
+Array.prototype.uniques = function() {
+	var n = {},r=[];
+	for(var i = 0; i < this.length; i++) {
+		if (!n[this[i]]) {
+			n[this[i]] = true;
+			r.push(this[i]);
+		}
+	}
+	return r;
 }
